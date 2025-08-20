9aff5fff0decc2667f4c056a2ec6540a38761158
ElrohirGT <elrohirgt@gmail.com>
2025-08-19 20:36:49 -0600

    feat: Added package and gitignore

--- package.nix
+++ package.nix
@@ -5,7 +5,7 @@
 buildGoModule {
   name = "Redes_MCPServer";
   src = ./.;
-  vendorHash = null;
+  vendorHash = "sha256-fTP/PZXcJUuDx3OA2zJSTqGTwcIAJI7qXeWlCit9f+k=";
   meta = {
     description = "FAGD MCP Server for Redes course";
     homepage = "https://github.com/ElrohirGT/Redes_MCPServer";
