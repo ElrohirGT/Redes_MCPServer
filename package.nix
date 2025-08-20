79126ee4cead85e1493633fe289925913f1e5b6f
ElrohirGT <elrohirgt@gmail.com>
2025-08-20 09:59:52 -0600

    fix: Packaging

--- package.nix
+++ package.nix
@@ -5,7 +5,9 @@
 buildGoModule {
   name = "Redes_MCPServer";
   src = ./.;
-  vendorHash = "sha256-fTP/PZXcJUuDx3OA2zJSTqGTwcIAJI7qXeWlCit9f+k=";
+  # vendorHash = "sha256-fTP/PZXcJUuDx3OA2zJSTqGTwcIAJI7qXeWlCit9f+k=";
+  vendorHash = "sha256-8Vs08PkqO/voV08HThCr7lwQ7AeEa501qhgBTwWlxB4=";
+  doCheck = false;
   meta = {
     description = "FAGD MCP Server for Redes course";
     homepage = "https://github.com/ElrohirGT/Redes_MCPServer";
